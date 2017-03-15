edbfbc53860438e0d61d1d5964ddbc21e2cd73ae
Juan Baldelomar <Juan Baldelomar>
2017-03-14 21:22:00 -0600

    HOJA DE TRABAJO 3 -CORTOCIRCUITO LISTO \n Nombre: Juan Luis Baldelomar \n Carnet: 201403785

--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,9 +8,28 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
+    public String etqV;
+    public String etqF;
+    public int tipo;
 
-    public NodoC3D(String cad) {
+    public NodoC3D(String cad, int tipo) {
         this.cad = cad;
+        this.etqF = "";
+        this.etqV = "";
+        this.tipo = tipo;
+    }
+    
+    public NodoC3D(String cad, String etqV, String etqF) {
+        this.cad = cad;
+        this.etqV = etqV;
+        this.etqF = etqF;
+    }
+    
+    public NodoC3D(String etqV, String etqF, int tipo) {
+        
+        this.etqV = etqV;
+        this.etqF = etqF;
+        this.tipo = tipo;
     }
     
     public String getCad(){
